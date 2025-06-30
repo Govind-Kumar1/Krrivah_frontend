0d26f8a2ebeff3f111e51b0d7c079ed5e9147ed1
Govind Kumar <govimdkumar407@gmail.com>
2025-06-30 23:30:28 +0530

    nav updated

--- src/pages/Design.jsx
+++ src/pages/Design.jsx
@@ -3,11 +3,12 @@ import HeroDesign from '../components/DesignComponents/HeroDesign'
 import DesignSection from '../components/DesignComponents/DesignSection'
 import GallerySection from '../components/DesignComponents/GallerySection'
 import Navbar from '../components/Navbar'
+import Navbar2 from '../components/Navbar2'
 
 const Design = () => {
   return (
      <>
-     <Navbar/>
+     <Navbar2/>
      <HeroDesign/>
      <DesignSection/>
      <GallerySection/>
